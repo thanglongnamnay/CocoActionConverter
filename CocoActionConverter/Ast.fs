a8ff46f172e9792080f8df32e539c7b6ecd4780b
Nguyen Long <thanglongnamnay@gmail.com>
2022-01-26 17:42:22 +0700

    Remove AFuncCall
    There should be coco function call only, other types of function call might be supported in the future.

--- CocoActionConverter/Ast.fs
+++ CocoActionConverter/Ast.fs
@@ -4,14 +4,8 @@ type Ast =
     | ANumber of float
     | AString of string
     | AVariable of string
-    | AFuncCall of FuncCall
     | ACocoAction of CocoAction
     | ACallFunc of string
-and FuncCall = {
-    This: Ast
-    Name: string
-    Params: Ast list
-}
 and CocoAction = {
     Name: string
     Params: Ast list
