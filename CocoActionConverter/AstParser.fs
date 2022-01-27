2314a1391b9759a60fe033e6317fcc66f099ea13
Nguyen Long <thanglongnamnay@gmail.com>
2022-01-27 10:31:38 +0700

    Add parsers' custom label

--- CocoActionConverter/AstParser.fs
+++ CocoActionConverter/AstParser.fs
@@ -4,34 +4,34 @@ open FParsec
 
 module AstParser =
     let pAst, pAstRef = createParserForwardedToRef<Ast, unit>()
-    let charsTill str = CharParsers.charsTillString str false Int32.MaxValue
+    let charsTill str = CharParsers.charsTillString str false Int32.MaxValue <?> $"String till '{str}'"
     let pCoco = CharParsers.pstring "cc."
     let pNumber = CharParsers.pfloat |>> ANumber
-    let pString = CharParsers.pchar '"' >>. charsTill "\"" |>> AString
-    let pVariable = CharParsers.regex """^[_$a-z][\w$]*$""" |>> AVariable
+    let pString = CharParsers.pchar '"' >>. charsTill "\"" |>> AString <?> "Quoted string"
+    let pVariable = CharParsers.regex """^[_$a-z][\w$]*$""" |>> AVariable <?> "Any variable or property"
     let pCallFunc = CharParsers.pstring "cc.callFunc" >>. ParingParensParser.parser |>> ACallFunc
-    let pParenOpen = CharParsers.pchar '(' .>> (opt (CharParsers.pchar '['))
-    let pParenClose = (opt (CharParsers.pchar ']')) >>. CharParsers.pchar ')'
+    let pParenOpen = CharParsers.pchar '(' .>> (opt (CharParsers.pchar '[')) <?> "Open paren '(' or '(['"
+    let pParenClose = (opt (CharParsers.pchar ']')) >>. CharParsers.pchar ')' <?> "Close paren ')' or '])'"
     let pInsideParens parser = pParenOpen >>. parser .>> pParenClose
     let spaces = CharParsers.spaces
     let splitBy token parser = Primitives.between pParenOpen pParenClose (spaces >>. sepEndBy (parser .>> spaces) (CharParsers.pstring token >>. spaces))
-    let pParamsInsideParens = splitBy "," pAst
+    let pParamsInsideParens = splitBy "," pAst <?> "Params split by comma"
     let pCocoActionName = pCoco >>. charsTill "("
-    let pEasing = CharParsers.pstringCI ".easing(cc." >>. charsTill "(" .>>. pParamsInsideParens .>> CharParsers.pstring ")"
+    let pEasing = CharParsers.pstringCI ".easing(cc." >>. charsTill "(" .>>. pParamsInsideParens .>> CharParsers.pstring ")" <?> "Coco Easing action"
     let pRepeatForever = CharParsers.stringCIReturn ".repeatForever()" Forever
     let pRepeatLimit = CharParsers.pstringCI ".repeat" >>. pInsideParens pAst |>> Limit
     let pRepeat = pRepeatForever <|> pRepeatLimit
     let pCocoAction =
         tuple4 pCocoActionName pParamsInsideParens (opt pEasing) (opt pRepeat)
         |>> fun (name, list, easing, repeat) -> ACocoAction {
-        Name = name
-        Params = list
-        Easing = easing |> Option.map (fun (name, paramList) -> {
             Name = name
-            Params = paramList
-        })
-        Repeat = repeat
-    }
+            Params = list
+            Easing = easing |> Option.map (fun (name, paramList) -> {
+                Name = name
+                Params = paramList
+            })
+            Repeat = repeat
+        }
     pAstRef.Value <- Primitives.choice [
         pCallFunc
         pCocoAction
