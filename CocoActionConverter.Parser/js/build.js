dd4f7c3630248b2ec8c91168c19e09c29f36d362
Nguyen Long <thanglongnamnay@gmail.com>
2022-02-07 11:01:17 +0700

    remove js project

--- CocoActionConverter.Parser/js/build.js
+++
@@ -1,8 +0,0 @@
-import { build } from 'esbuild'
-build({
-    entryPoints: ['index.js'],
-    bundle: true,
-    watch: true,
-    platform: 'node',
-    outfile: 'bundle.cjs',
-  }).catch(() => process.exit(1))
--- CocoActionConverter.Parser/js/test.js
+++
@@ -1,5 +0,0 @@
-iconPileChip.runAction(cc.moveBy(0.3, 0, 20));
-        iconPileChip.runAction(cc.sequence([
-            cc.rotateBy(0.15, 5),
-            cc.rotateBy(0.15, -5),
-        ]).repeatForever());
